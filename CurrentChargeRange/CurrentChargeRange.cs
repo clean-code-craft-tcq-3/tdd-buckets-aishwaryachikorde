40814fc7706e9c9d9f925069dc01e7ddd09219ca
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-06-20 00:11:09 +0530

    Create CurrentChargeRange.cs
    Created a backend class which refers to the method SortInputArray in Test class.

---
+++ CurrentChargeRange/CurrentChargeRange.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace CurrentChargeRanges
+{
+  public class CurrentChargeRange
+  {
+    public static List<int> SortInputArray(int[] inputNumbers)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
