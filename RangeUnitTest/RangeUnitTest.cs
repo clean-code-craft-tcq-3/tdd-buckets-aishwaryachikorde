cbb72da3a78fdab78765de602fc9e9b6e27d41f6
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-06-30 00:30:38 +0530

    Update RangeUnitTest.cs

--- RangeUnitTest/RangeUnitTest.cs
+++ RangeUnitTest/RangeUnitTest.cs
@@ -9,23 +9,6 @@ namespace RangeUnitTest
   public class RangeUnitTest
   {
     [TestMethod]
-    public void TestSortInputArray()
-    {
-      //Positive scenario
-      List<int> sortInputList = CurrentChargeRange.SortInputArray(new[] { 1, 3, 4, 2, 6, 5 });
-      CollectionAssert.AreEqual(sortInputList, new[]{ 1, 2,3,4,5,6 });
-
-      List<int> sortSmallestList = CurrentChargeRange.SortInputArray(new[] { 4, 5 });
-      CollectionAssert.AreEqual(sortSmallestList, new[] { 4, 5});
-
-      List<int> sortNegativeList = CurrentChargeRange.SortInputArray(new[] { -1, -2, 0, 1, 5, 6 });
-      CollectionAssert.AreEqual(sortNegativeList, new[] {-2,-1, 0, 1, 5, 6 });
-
-      //Negative Scenario
-      Assert.ThrowsException<NullReferenceException>(() => CurrentChargeRange.SortInputArray(null));
-    }
-
-    [TestMethod]
     public void TestGetRangeReading()
     {
       //Test with one group of consecutive numbers
